3ce96792fe3b22f04f3185dadfa0aa96871124be
Matthew Wicker <mdwicker@gmail.com>
2021-01-01 17:46:59 -0800

    Clean up code, start coding operators

--- script.js
+++ script.js
@@ -1,3 +1,109 @@
+// operation[0] is the first number, 
+// operation [1] is the operator, 
+// operator [2] is the second number
+let operation = [];
+
+// stores the current number on the screen
+let displayNumber = "";
+
+// variable to update the screen content
+const screen = document.getElementById("screen");
+
+// when a button is pressed, parse the input
+const buttons = document.querySelectorAll("button");
+buttons.forEach(button => {
+    button.addEventListener("click", parseInput);
+})
+
+function parseInput(e) {
+    // check what type of input it is
+    inputType = getInputType(this);
+
+    // get the specific calculator key
+    let keyValue = this.id;
+
+    switch(inputType) {
+        case "equals" : 
+            equals();
+        case "number" :
+            addNumber(keyValue);
+        case "edit" :
+            edit(keyValue);
+        case "operator" :
+            parseOperator(keyValue);
+    }
+
+    updateDisplay();
+}
+
+// process an equals key
+function equals() {
+    // add the latest number to the operation
+    operation.push(displayNumber);
+
+    // if all three arguments are present, perform the operation
+}
+
+function getInputType(key) {
+    if (key.id === "equals") {
+        return "equals";
+    } else if (key.classList.contains("operator")) {
+        return "operator";
+    } else if (key.classList.contains("number")) {
+        return "number";
+    } else if (key.classList.contains("edit")) {
+        return "edit";
+    }
+
+    return "unknown";
+}
+
+function addNumber(number) {
+    if (number === "dot") {
+        // only allow one decimal point
+        if (displayNumber.indexOf(".") !== -1) {
+            return;
+        } else {
+            displayNumber += displayNumber ? "." : "0."; 
+        }
+    } else {
+        displayNumber += number;
+    } 
+}
+
+function edit(editType) {
+    if (editType === "clear") {
+        displayNumber = ""
+    }
+    if (editType === "backspace") {
+        displayNumber = displayNumber.slice(0,-1);
+    }
+}
+
+function parseOperator(operatorType) {
+    // if there is no number to operate on, operate on 0
+    if (!operation[0]) {
+        operation[0] = 0;
+    }
+
+    // if the operation is already ready to go, process the existing one first
+    else if (operation[2]) {
+        checkOperation();
+    }
+
+    else {
+        operation.push(operatorType);
+    }
+}
+
+function updateDisplay() {
+    // don't allow more than 8 characters
+    displayNumber.length >= 8 ? displayNumber = displayNumber.slice(0, 8) : "";
+
+    // show the working number on the screen. If there is no working number, show "00"
+    screen.textContent = displayNumber ? displayNumber : "00";
+}
+
 // basic math functions
 function add(x, y) {
     return x + y;
